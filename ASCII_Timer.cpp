29084dc07503061b71e44c84a9781e4d7e2ce66e
Valentin Krumov <valentinvpk@abv.bg>
2020-12-22 21:48:15 +0200

    Added user validation.

---
+++ ASCII_Timer.cpp
@@ -0,0 +1,24 @@
+﻿
+#include <iostream>
+using namespace std;
+
+bool isNumberCorrect(int number) {
+    if (number < 1000 || number > 9999) {
+        return false;
+    }
+
+    return true;
+}
+
+int main()
+{
+    int seconds;
+    cin >> seconds;
+
+    if (!isNumberCorrect) {
+        cout << "-1";
+        return 0;
+    }
+
+}
+
