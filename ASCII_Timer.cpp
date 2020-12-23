fd737680a4d15dd71d17c308b5919e568cc2fa2c
Valentin Krumov <valentinvpk@abv.bg>
2020-12-23 22:15:02 +0200

    Added timer.

--- ASCII_Timer.cpp
+++ ASCII_Timer.cpp
@@ -1,5 +1,8 @@
 ﻿
 #include <iostream>
+#include <stdlib.h>
+#include <windows.h>
+
 using namespace std;
 
 bool isNumberCorrect(int number) {
@@ -10,15 +13,39 @@ bool isNumberCorrect(int number) {
     return true;
 }
 
+void timer(int minutes, int seconds) {
+
+    while (true) {
+        if (minutes == 0 && seconds == 0) {
+            break;
+        }
+
+        if (seconds == 0) {
+            seconds = 60;
+            minutes--;
+        }
+
+        system("cls");
+        cout << minutes << ":" << seconds--;
+        Sleep(1000);
+    }
+}
+
 int main()
 {
-    int seconds;
-    cin >> seconds;
+    int number;
+    cin >> number;
 
     if (!isNumberCorrect) {
         cout << "-1";
         return 0;
     }
 
+    int minutes = number / 60;
+    int seconds = number % 60;
+
+    timer(minutes, seconds);
+
+    return 0;
 }
 
